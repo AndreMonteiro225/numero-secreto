3ea6c9cb58a0cfc63e9110c3094145c6afb6cb7f
André Monteiro <andremonteiro225@gmail.com>
2023-10-17 22:57:09 -0300

    projeto inicial

---
+++ cursoLogicaJS/teste7DaysOfCode.js
@@ -0,0 +1,25 @@
+
+
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+
+if (numeroUm === stringUm) {
+    print('numeros iguais de tipos iguais');
+} else {
+    print('numeros iguais de tipos diferentes');
+}
+
+function comparaN30() {
+    if (numTrinta === stringTrinta) {
+        print('numeros iguais de tipos iguais');
+    } else {
+        print('numeros iguais de tipos diferentes');
+    }
+}
+
+comparaN30();
---
+++ maybe works/comparador de tipos.js
@@ -0,0 +1,27 @@
+//comparador de numeros
+
+let = teste;
+let numUm = 1
+let stringUm = '1'
+let numTrinta = 30
+let stringTrinta = '30'
+let numDez = 10
+let stringDez ='10'
+
+function comparador(){
+if(numUM === stringUm){
+    console.log('as variaveis sao igualis, mas tem tipos diferentes.')
+} else {
+    console.log('as variaveis sao de tipos diferentes.')
+    }
+}
+
+function teste(){
+    console.log('oi')
+}   
+
+return teste( );
+
+
+
+
